030d9442a0679376d9044c92c3e701859a342f11
MV2322 <126760558+MV2322@users.noreply.github.com>
2024-08-12 13:46:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ provider "aws" {
 }
 
 # ECS Cluster
-resource "aws_ecs_cluster" "mycluster" {
-  name = "my-ecs-cluster"
+resource "aws_ecs_cluster" "mycluster1" {
+  name = "my-ecs-cluster1"
 }
 
 # ECS Task Definition
-resource "aws_ecs_task_definition" "my-tdf" {
-  family = "my-task"
+resource "aws_ecs_task_definition" "my-tdef" {
+  family = "my-task1"
   container_definitions = jsonencode([
     {
-     name      = "my-container"
+     name      = "my-container1"
      image     = "nginx:latest"
      cpu       = 256
      memory    = 512
@@ -31,55 +31,55 @@ resource "aws_ecs_task_definition" "my-tdf" {
 }
 
 # Load Balancer
-resource "aws_lb" "my-loadbalancer" {
-  name               = "my-lb"
+resource "aws_lb" "my-loadbalancer1" {
+  name               = "my-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["sg-0899928ebfbbdaf82"]
+  security_groups    = ["sg-0d7ebec7909508cc2"]
   subnets            = [
-    "subnet-08774b4f299c3f5ef",  # Replace with your first subnet ID in a different AZ
-    "subnet-00308d31ff7843ef4"   # Replace with your second subnet ID in a different AZ
+    "subnet-01476685f9318d9a9",  # Replace with your first subnet ID in a different AZ
+    "subnet-0a2ea30bc96e7e090"   # Replace with your second subnet ID in a different AZ
   ]
 }
 
 
 # Target Group
-resource "aws_lb_target_group" "my-tg" {
-  name     = "my-target-group"
+resource "aws_lb_target_group" "my-tg1" {
+  name     = "my-target-group1"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-0c28b7caa4a8934e1"  # Replace with your VPC ID
+  vpc_id   = "vpc-0ea0b66401c8aba3b"  # Replace with your VPC ID
   target_type = "instance"
 }
 
 # Load Balancer Listener
 resource "aws_lb_listener" "my-listener" {
-  load_balancer_arn = aws_lb.my-loadbalancer.arn
+  load_balancer_arn = aws_lb.my-loadbalancer1.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.my-tg.arn
+    target_group_arn = aws_lb_target_group.my-tg1.arn
   }
 }
 
 # ECS Service
 resource "aws_ecs_service" "my-svc" {
   name            = "my-service"
-  cluster         = aws_ecs_cluster.mycluster.id
-  task_definition = aws_ecs_task_definition.my-tdf.arn
+  cluster         = aws_ecs_cluster.mycluster1.id
+  task_definition = aws_ecs_task_definition.my-tdef.arn
   desired_count   = 1
 
   network_configuration {
-    subnets         = ["subnet-08774b4f299c3f5ef", "subnet-00308d31ff7843ef4"]
-    security_groups = ["sg-0899928ebfbbdaf82"]
+    subnets         = ["subnet-01476685f9318d9a9", "subnet-0a2ea30bc96e7e090"]
+    security_groups = ["sg-0d7ebec7909508cc2"]
     assign_public_ip = true
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.my-tg.arn
-    container_name   = "my-container"
+    target_group_arn = aws_lb_target_group.my-tg1.arn
+    container_name   = "my-container1"
     container_port   = 80
   }
 
@@ -90,6 +90,6 @@ resource "aws_ecs_service" "my-svc" {
 
 # Output the DNS name of the Load Balancer
 output "load_balancer_dns_name" {
-  value = aws_lb.my-loadbalancer.dns_name
+  value = aws_lb.my-loadbalancer1.dns_name
 }
 
